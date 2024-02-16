7a83acc95f59bdcc9113baa64b2cf4a4c96f7aa5
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-02-16 02:45:35 +0100

    Added functionality to toggle farming controls between arrow keys and WASD keys

--- src/main/kotlin/dulkirmod/command/FarmingControlSchemeCommand.kt
+++ src/main/kotlin/dulkirmod/command/FarmingControlSchemeCommand.kt
@@ -25,19 +25,38 @@ class FarmingControlSchemeCommand : ClientCommandBase("farmcontrols") {
             val minecraft = Minecraft.getMinecraft()
             val breakingKey: KeyBinding = minecraft.gameSettings.keyBindAttack
             val jumpKey: KeyBinding = minecraft.gameSettings.keyBindJump
+            val forwardKey: KeyBinding = minecraft.gameSettings.keyBindForward
+            val backwardKey: KeyBinding = minecraft.gameSettings.keyBindBack
+            val leftKey: KeyBinding = minecraft.gameSettings.keyBindLeft
+            val rightKey: KeyBinding = minecraft.gameSettings.keyBindRight
+
             if (!enabled) {
                 KeyBinding.setKeyBindState(breakingKey.keyCode, false)
-                breakingKey.keyCode = 57 // 57 = space key code
+                breakingKey.keyCode = 57 // 57 = Space key code
 
                 KeyBinding.setKeyBindState(jumpKey.keyCode, false)
                 jumpKey.keyCode = -100 // -100 = Left click key code
+
+                // Arrow key codes
+                forwardKey.keyCode = 200 // 200 = Arrow Up key code
+                backwardKey.keyCode = 208 // 208 = Arrow Down key code
+                leftKey.keyCode = 203 // 203 = Arrow Left key code
+                rightKey.keyCode = 205 // 205 = Arrow Right key code
+
                 minecraft.gameSettings.mouseSensitivity = 0.0f
             } else {
                 KeyBinding.setKeyBindState(breakingKey.keyCode, false)
                 breakingKey.keyCode = -100 // -100 = Left click key code
 
                 KeyBinding.setKeyBindState(jumpKey.keyCode, false)
-                jumpKey.keyCode = 57 // 57 = space key code
+                jumpKey.keyCode = 57 // 57 = Space key code
+
+                // Restore default key codes
+                forwardKey.keyCode = 17 // 17 = W key code
+                backwardKey.keyCode = 31 // 31 = S key code
+                leftKey.keyCode = 30 // 30 = A key code
+                rightKey.keyCode = 32 // 32 = D key code
+
                 minecraft.gameSettings.mouseSensitivity = DulkirConfig.defaultSens / 2
             }
 
@@ -46,7 +65,7 @@ class FarmingControlSchemeCommand : ClientCommandBase("farmcontrols") {
             minecraft.gameSettings.loadOptions()
 
             enabled = !enabled
-            TextUtils.toggledMessage("Farming Controls", enabled)
+            TextUtils.toggledMessage("lil bru i think you just enabled the farming controls :3 oil up or else im gonna do something really to u :D", enabled)
         }
     }
 }

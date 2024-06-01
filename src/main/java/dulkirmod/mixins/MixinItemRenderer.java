69cb0c00075b3bd820e704b51fa3abb85b001f54
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-06-01 01:43:45 +0100

    Remove Unused Code

--- src/main/java/dulkirmod/mixins/MixinItemRenderer.java
+++ src/main/java/dulkirmod/mixins/MixinItemRenderer.java
@@ -3,9 +3,7 @@ package dulkirmod.mixins;
 import dulkirmod.features.ItemAnimations;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,10 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = {ItemRenderer.class}, priority = 1010)
 public class MixinItemRenderer {
-
-    @Shadow
-    @Final
-    private RenderItem itemRenderer;
 
     @Shadow
     private ItemStack itemToRender;
